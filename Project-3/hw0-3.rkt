ceeb62bc13f4e2863aeb644e791738b16ac67502
kekekek <katiakrut448@gmail.com>
2023-09-27 13:25:19 +0300

    Add files via upload

---
+++ Project-3/hw0-3.rkt
@@ -0,0 +1,74 @@
+#lang racket
+
+(require (planet dyoo/simply-scheme))
+(provide (all-defined-out))
+
+; Exercise 0
+(define (count-ums sent)
+  ; your code here
+ (error "Not yet implemented")
+)
+
+(define (countdown num)
+  ; your code here
+ (error "Not yet implemented")
+)
+
+(define (numbers sent)
+  ; your code here
+ (error "Not yet implemented")
+)
+
+; Exercise 1 - Define describe-time
+(define (describe-time secs)
+  ; your code here
+ (error "Not yet implemented")
+)
+
+; Exercise 2 - Define remove-once
+(define (remove-once wd sent)
+  ; your code here
+ (error "Not yet implemented")
+)
+
+; Exercise 3 - Define differences
+(define (differences nums)
+  ;your code here
+ (error "Not yet implemented")
+)
+
+; Exercise 4 - Define location
+(define (location small big)
+  ; your code here
+ (error "Not yet implemented")
+)
+
+; Exercise 5 - Define initials
+(define (initials sent)
+  ; your code here
+ (error "Not yet implemented")
+)
+
+; Exercise 6 - Define copies
+(define (copies num wd)
+  ; your code here
+ (error "Not yet implemented")
+)
+
+; Exercise 7 - Define gpa
+(define (gpa grades)
+  ; your code here
+ (error "Not yet implemented")
+)
+
+; Exercise 8 - Define repeat-words
+(define (repeat-words sent)
+  ; your code here
+ (error "Not yet implemented")
+)
+
+; Exercise 9 - Define same-shape?
+(define (same-shape? sent1 sent2)
+  ; your code here
+ (error "Not yet implemented")
+)
