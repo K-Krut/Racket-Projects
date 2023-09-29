932edaa25256c637ca259dd6bf7d863715b8a8fd
kekekek <katiakrut448@gmail.com>
2023-09-29 11:53:37 +0300

    Add files via upload

--- Project-2/hw0-2.rkt
+++ Project-2/hw0-2.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
-(require berkeley)
+;; (require berkeley)
+(require (planet dyoo/simply-scheme))
 (provide (all-defined-out))
 
 ;Exercise 0
@@ -8,13 +9,13 @@
 ;1. Atom
 10
 ;2. Compound Expression (3 Atoms)
-(+ 3 7)
+(+ 6 4)
 ;3. Compound Expression (4 Atoms)
-(+ 2 2 6)
+(+ 6 3 1)
 ;4. Compound Expression (1 Atom and 2 subexpressions)
-(+ (* 1 4) (* 2 3))
+(+ (* 2 2) (* 3 2))
 ;5. Any Other Kind Expression
-(- 20 (* 2 5))
+(- 11 1)
 
 ;Exercise 1
 (define (second wd)
@@ -22,69 +23,76 @@
 
 ;1. Define first-two
 (define (first-two wd)
+  ; your code here
   (word (first wd) (second wd))
-  ; (error "Not yet implemented")
 )
 
 ;;2. Define two-first
 (define (two-first x y)
   ; your code here
-  (error "Not yet implemeted")
-)
+  (word (first x) (first y)))
 
 ;;3. Define two-first-sent
 (define (two-first-sent sent)
   ; your code here
-  (error "Not yet implemented")
-)
+  (word (first (first sent)) (first (second sent))))
 
 ;Exercise 2 - Define teen?
 (define (teen? num)
   ; your code here
-  (error "Not yet implemented")
-)
+  (and (<= num 19) (>= num 13)))
 
 ;Exercise 3 - Define indef-article
 (define (indef-article wd)
   ; your code here
-  (error "Not yet implemented")
-)
+  (if (member (first wd) '(a e u o i))
+      (se 'an wd)
+      (se 'a wd)))
 
 ;Exercise 4 - Define insert-and
 (define (insert-and sent)
   ; your code here
-  (error "Not yet implemented")
-)
+  (se (bl sent) 'and (last sent)))
 
 ;Exercise 5 - Define query
 (define (query sent)
   ; your code here
-  (error "Not yet implemented")
-)
+  (se (second sent) (first sent) (bl (bf (bf sent))) (word (last sent) '?)))
 
 ;Exercise 6 - Define european-time and american-time
 (define (european-time time)
   ; your code here
-  (error "Not yet implemented")
-)
+  (cond ((equal? time '(12 am)) 0)
+        ((equal? time '(12 pm)) 12)
+        ((equal? (last time) 'pm) (+ (first time) 12))
+        (else (first time))))
 
 (define (american-time time)
   ; your code here
-  (error "Not yet implemented")
-)
+  (cond ((equal? time 0) '(12 am))
+        ((equal? time 12) '(12 pm))
+        ((> time 12) (se (- time 12) 'pm))
+        (else (se time 'am))))
 
 ;Exercise 7 - Define describe-time
 (define (describe-time secs)
   ; your code here
-  (error "Not yet implemented")
-)
+  (cond ((< secs 60) (se secs 'seconds))
+        ((< secs (* 60 60)) (se (/ secs 60.0) 'minutes))
+        ((< secs (* 60 60 24)) (se (/ secs (* 60.0 60)) 'hours))
+        ((< secs (* 60 60 24 365.25)) (se (/ secs (* 60.0 60 24)) 'days))))
 
 ;Exercise 8 - Explain why superlative doesnt work:
-(define (superlative adjective word)
-  (se (word adjective 'est) word))
+; (define (superlative adjective word)
+;   (se (word adjective 'est) word))
+
+(define (superlative adjective wd)
+  (se (word adjective 'est) wd))
 
 #|
 
 Explanation here.
-
+The built-in procedure "word" has been replaced with whatever was passed as the second
+argument to "superlative". Thus, the "word" built-in is no longer available within the
+scope of the procedure. Change the "word" variable in the superlative procedure to fix
 |#
