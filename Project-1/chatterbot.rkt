96b48e63c93abb7e0161ecac8d0c879db1bb20e8
K-Krut <katiakrut448@gmail.com>
2023-11-22 22:54:38 +0200

    project-1 bot functions

--- Project-1/chatterbot.rkt
+++ Project-1/chatterbot.rkt
@@ -5,10 +5,10 @@
 
 ;;Begin Project 1
 (require "adjectives.rkt")
+(require "logic.rkt")
 
 ;;Below is some procedures to help you out. Dont worry about what it says or does.
 ;;Questions are below.
-
 (define (want-exit? line)
   (or (member? 'exit line) (member? 'quit line) (member? 'bye line)))
 
@@ -51,63 +51,91 @@
 ;;Q1 - babybot
   (define (babybot sent)
     ;;insert your answer here
-    (error "not yet implemented")
-  )
+    sent)
+
 
 ;;Q2 - stupidbot-creator
   (define (stupidbot-creator motto)
     ;;insert your answer here
-    (error "not yet implemented")
-  )
+    (lambda (sent) motto))
+
 
 ;;Q3 - matcherbot-creator
   (define (matcherbot-creator pattern)
     ;;insert your answer here
-    (error "not yet implemented")
-  )
+    (lambda (sent) (last (pattern-matcher pattern sent))))
+
 
 ;;Q4 - substitutebot-creator
   (define (substitutebot-creator from to)
     ;;insert your answer here
-    (error "not yet implemented")
-  )
+    (define (substitute-bot sent)
+      (if  (empty? sent)
+           '()
+           (se (check-for-replace (first sent) from to) (substitute-bot (bf sent)))))
+    (lambda (arg-sent) (substitute-bot arg-sent)))
+
 
 ;;Q5 - switcherbot
   (define (switcherbot sent)
     ;;insert your answer here
-    (error "not yet implemented")
-  )
+    (define (switch-helper-func arg-sent)
+      (if  (empty? arg-sent)
+           '()
+           (se (check-for-replace (first arg-sent) '(me I am was my yours you are were your mine) '(you you are were your mine me am was my yours)) (switch-helper-func (bf arg-sent)))))
+    (if (equal? (first sent) 'you)
+          (se 'I (switch-helper-func (bf sent)))
+          (switch-helper-func sent)))
 
 
 ;;Q6 - inquisitivebot
   (define (inquisitivebot sent)
     ;;insert your answer here
-    (error "not yet implemented")
-  )
+    (if (empty? sent)
+        '()
+        (se (switcherbot sent) '?)))
+
 
 ;;Q7 - eliza
   (define (eliza sent)
     ;;insert your answer here
-    (error "not yet implemented")
-  )
+    (cond
+      ((empty? sent) '(how can I help you ?))
+      ((list? (first (pattern-matcher '(hello) sent))) '(hello there!))
+      ((equal? (last sent) '?) '(I can not answer your question.))
+      ((and (>= (count sent) 2) (equal? (se (first sent) (first (bf sent))) '(I am))) (se '(why are you) (bf (bf sent)) '?))
+      (else  (switcherbot sent))))
+
 
 ;;Q8 - reactorbot-creator
   (define (reactorbot-creator bot pat out)
     ;;insert your answer here
-    (error "not yet implemented")
-  )
+    (define (reactorbot sent)
+      (if (equal? sent pat)
+       out
+       (bot sent)))
+    reactorbot)
 
 ;;Q9 - replacerbot-creator
   (define (replacerbot-creator bot pat before after)
     ;;insert your answer here
-    (error "not yet implemented")
-  )
+    (define (replacerbot sent)
+      (define check-pattern (pattern-matcher pat sent))
+      (if (list? (first check-pattern))
+       (se before (last check-pattern) after)
+       (bot sent)))
+    replacerbot)
 
 ;;Q10 - exagerate
   (define (exaggerate bot n)
     ;;insert your answer here
-    (error "not yet implemented")
-  )
-
-;;REMEMBER TO ADD YOUR OWN TESTS TO GRADER.RKT!
-;;END OF PROJECT 1
+    (define (exaggerate-helper arg-sent)
+      (cond
+        ((empty? arg-sent) '())
+        ((adjective? (first arg-sent)) (se 'very (first arg-sent) (exaggerate-helper (bf arg-sent))))
+        (else (se (first arg-sent) (exaggerate-helper (bf arg-sent))))))
+    (define (helper-function arg-n x arg-proc arg-proc-additional)
+      (if (<= arg-n 0)
+        x
+        (arg-proc-additional (arg-proc (helper-function (- arg-n 1) x arg-proc arg-proc-additional)))))
+    (lambda (arg-sent) (helper-function n arg-sent bot exaggerate-helper)))
